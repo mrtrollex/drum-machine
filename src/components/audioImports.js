6d3b77c552c606279b1f57e43dc4ce0bbfe8e2d1
mrtrollex <619ricardo619@gmail.com>
2021-09-14 15:58:02 +0200

    added audios and imported them

---
+++ src/components/audioImports.js
@@ -0,0 +1,19 @@
+import heaterOneAudio from "../audio/Heater-1.mp3";
+import heaterTwoAudio from "../audio/Heater-2.mp3";
+import heaterThreeAudio from "../audio/Heater-3.mp3";
+import heaterFourAudio from "../audio/Heater-4_1.mp3";
+import clapAudio from "../audio/Heater-6.mp3";
+import openHHAudio from "../audio/Dsc_Oh.mp3";
+import kickAndHatAudio from "../audio/Kick_n_Hat.mp3";
+import kickAudio from "../audio/RP4_KICK_1.mp3";
+import closedHHAudio from "../audio/Cev_H2.mp3";
+
+export const heaterOne = heaterOneAudio;
+export const heaterTwo = heaterTwoAudio;
+export const heaterThree = heaterThreeAudio;
+export const heaterFour = heaterFourAudio;
+export const clap = clapAudio;
+export const openHH = openHHAudio;
+export const kickAndHat = kickAndHatAudio;
+export const kick = kickAudio;
+export closedHH = closedHHAudio;
